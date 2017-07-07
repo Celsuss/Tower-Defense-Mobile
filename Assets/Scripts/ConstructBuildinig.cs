9ecdfa7d435a02bc9201472e7b835f6753f0705d
Celsuss <Celsuss@hotmail.com>
2017-07-07 03:30:52 +0200

    Fixed bugg, can no longer place more than one building on the same tile

--- Assets/Scripts/ConstructBuildinig.cs
+++ Assets/Scripts/ConstructBuildinig.cs
@@ -26,6 +26,11 @@ public class ConstructBuildinig : MonoBehaviour {
 
 	void AddBuuilding(){
 		GameObject tile = m_BoardManager.GetTileClosestToMouse();
+		for(int i = 0; i < tile.transform.childCount; i++){
+			if( tile.transform.GetChild( i ).tag == "Building" )
+				return;
+		}
+
 		GameObject newTile = Instantiate( m_Prefab, tile.transform.position, tile.transform.rotation, tile.transform );
 	}
 }
